ef93e2cdab1ffafb50c4ffb5059fee929a6a39d3
short21 <hluas.qus.dawb@gmail.com>
2022-07-05 10:47:56 +0000

    Add

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'users/index'
+    get 'users/show'
+    get 'users/edit'
+  end
+  namespace :public do
+    get 'users/show'
+    get 'users/edit'
+  end
   # 会員用
 # URL /customers/sign_in ...
 devise_for :users,skip: [:passwords], controllers: {
@@ -15,14 +24,19 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
 
   ########### 会員側のルーティング設定 ##########
   namespace :public do
-    resources :posts
+    resources :users, only: [:show, :edit, :destroy, :update]
+    resources :posts, only: [:new, :create, :index, :show, :destroy] do
+      resources :post_comments, only: [:create]
+    end
 
   end
 
 
   ########## 管理者側のルーティング設定 ##########
   namespace :admin do
-    resources :posts
-
+    resources :users, only: [:index, :show, :edit, :destroy, :update]
+    resources :posts, only: [:new, :create, :index, :show, :destroy] do
+      resources :post_comments, only: [:create]
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_05_100218) do
+ActiveRecord::Schema.define(version: 2022_07_05_102405) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 2022_07_05_100218) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "post_comments", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.string "comment", default: "", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "posts", force: :cascade do |t|
