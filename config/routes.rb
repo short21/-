07f913c2c3fe4e6f70ee99885565db37d5b557ac
short21 <hluas.qus.dawb@gmail.com>
2022-07-05 09:52:04 +0000

    Add

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,25 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  # 会員用
+# URL /customers/sign_in ...
+devise_for :users,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  ########### 会員側のルーティング設定 ##########
+  namespace :public do
+  end
+  
+
+  ########## 管理者側のルーティング設定 ##########
+  namespace :admin do
+
+  end
 end
