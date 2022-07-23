3481710c0b17905652079f890842abcbf4dc1714
short21 <hluas.qus.dawb@gmail.com>
2022-07-23 15:46:24 +0000

    Fix

--- db/migrate/20220705100218_create_posts.rb
+++ db/migrate/20220705100218_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-      t.integer :user_id,              null: false, default: ""
-      t.string :body,              null: false, default: ""
+      t.integer :user_id
+      t.string :body
       t.timestamps
     end
   end
