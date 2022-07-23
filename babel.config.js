cb5d3944cee40d94af63294cb76fa619a7c73553
tomoaki-kimura <tkim19750404@gmail.com>
2022-07-23 23:00:13 +0900

    fix bootstrap files

--- babel.config.js
+++ babel.config.js
@@ -54,21 +54,11 @@ module.exports = function(api) {
         }
       ],
       [
-        '@babel/plugin-proposal-private-methods',
-        {
-          loose: true
-        }
-      ],
-      [
-        '@babel/plugin-proposal-private-property-in-object',
-        {
-          loose: true
-        }
-      ],
-      [
         '@babel/plugin-transform-runtime',
         {
-          helpers: false
+          helpers: false,
+          regenerator: true,
+          corejs: false
         }
       ],
       [
@@ -76,7 +66,9 @@ module.exports = function(api) {
         {
           async: false
         }
-      ]
+      ],
+      ["@babel/plugin-proposal-private-methods", { "loose": true }],
+      ["@babel/plugin-proposal-private-property-in-object", { "loose": true }]
     ].filter(Boolean)
   }
 }
