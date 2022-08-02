5534317ead7d00726f5b7149de4169cebae74f50
short21 <hluas.qus.dawb@gmail.com>
2022-08-02 13:18:31 +0000

    Add

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -41,8 +41,8 @@ class Public::PostsController < ApplicationController
   end
 
   def require_access_time
-    if 23 <= Time.current.hour || Time.current.hour <= 5
-      #redirect_to times_path
+    if 21 <= Time.current.hour || Time.current.hour <= 5
+      redirect_to times_path
     end
   end
 end
